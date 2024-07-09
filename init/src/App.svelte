33fa852b5e3b1a6737252345852abcec746ade68
dimas <dimas.priyo@bluebirdgroup.com>
2024-07-09 09:21:08 +0700

    add landing page

---
+++ init/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import "bootstrap/dist/css/bootstrap.min.css";
+</script>
+
+<div class="mx-auto pt-5" style="width: 500px;">
+	<div class="row">
+		<div
+			class="card text-white bg-primary mb-3 col-sm"
+			style="max-width: 18rem;"
+		>
+			<a
+				href="https://dimaspriyo.github.io/json-format/"
+				class="text-white"
+			>
+				<div class="card-header">Json Formatter</div>
+				<div class="card-body">
+					<h5 class="card-title">
+						Tools for unescape and format json
+					</h5>
+					<!-- <p class="card-text">Tools for unescape and format json</p> -->
+				</div>
+			</a>
+		</div>
+		<div class="col-sm"></div>
+		<div
+			class="card text-white bg-secondary mb-3 col-sm"
+			style="max-width: 18rem;"
+		>
+			<a href="https://dimaspriyo.github.io/makan-apa" class="text-white">
+				<div class="card-header">Makan Apa</div>
+				<div class="card-body">
+					<h5 class="card-title">Bingung Mau Makan</h5>
+					<!-- <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p> -->
+				</div>
+			</a>
+		</div>
+	</div>
+</div>
